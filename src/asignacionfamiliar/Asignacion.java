f8df140b5714a6672e987c246b7ee69d9172a9e4
Leandro <Leandro@DESKTOP-785HDEB>
2022-10-10 10:59:42 -0300

    test

---
+++ src/asignacionfamiliar/Asignacion.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package asignacionfamiliar;
+
+/**
+ *
+ * @author Leandro
+ */
+class Asignacion {
+
+    String tipo;
+    int ID;
+    int DNIFamiliar;
+    int fechaVencimiento;
+    
+    
+}
